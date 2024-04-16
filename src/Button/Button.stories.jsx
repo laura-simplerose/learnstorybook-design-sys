bd9a07a755e0f0ad260d7ca72cf38d8288068ace
laura-simplerose <laura@simplerose.com>
2024-04-16 12:48:02 -0400

    distribution step of developer tutorial

--- src/Button/Button.stories.jsx
+++ src/Button/Button.stories.jsx
@@ -4,6 +4,7 @@ import { Button } from './Button';
 import { Icon } from '../Icon/Icon';
 import { StoryLinkWrapper } from '../LinkWrapper';
 
+import { expect, userEvent, within } from '@storybook/test';
 
 const CustomButton = styled.button`
   border: 1px solid green;
@@ -21,6 +22,8 @@ export default {
   title: 'Design System/Button',
   component: Button,
 };
+
+
 
 export const AllButtons = {
   name: 'all buttons',
@@ -327,3 +330,25 @@ export const AnchorWrapper = {
   ),
 };
 
+/*
+ * New story using the play function.
+ * See https://storybook.js.org/docs/react/writing-stories/play-function
+ * to learn more about the play function.
+ */
+ export const WithInteractions = {
+   args: {
+     appearance: 'primary',
+     href: 'http://storybook.js.org',
+     ButtonWrapper: StoryLinkWrapper,
+     children: 'Button',
+   },
+   play: async ({ canvasElement }) => {
+     // Assigns canvas to the component root element
+     const canvas = within(canvasElement);
+     await userEvent.click(canvas.getByRole('link'));
+     expect(canvas.getByRole('link')).toHaveAttribute(
+       'href',
+       'http://storybook.js.org',
+     );
+   },
+ };
